dcd2a755cb40ea5f930fac50bc699a77e349205e
Vitor <vitor.turqueti@aida.digital>
2023-10-29 00:41:03 -0300

    backend: evento create_game agora emite o evento game_created para indicar para o cliente a sala em que ele será conectado; new_message agora é emitido para a sala da mensagem recebida

--- api/routes/socket.py
+++ api/routes/socket.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request
 from flask_socketio import SocketIO, join_room, emit
 from server.instance import server
 from routes.routes import games, get_next_id
@@ -8,18 +8,25 @@ from models.player import Player
 messageCount = 0
 players = []
 
+
 @server.socketio.on('connect')
 def test_connect():
     print('connection received')
+
 
 @server.socketio.on('create_game')
 def create(data):
     username = data['username']
     id = get_next_id()
     players.append(1)
-    games.append(Game(id,Player(len(players), username,request.sid)))
+    games.append(Game(id, Player(len(players), username, request.sid)))
     join_room(id)
-    server.socketio.emit('room_message',f'{username} has intered on room {id}',to=id)
+    server.socketio.emit(
+        'room_message', f'{username} has intered on room {id}', to=id)
+    server.socketio.emit(
+        'game_created', {'room': id}, to=request.sid
+    )
+
 
 @server.socketio.on('connect_game')
 def create(data):
@@ -27,10 +34,11 @@ def create(data):
     id = int(data['room'])
 
     players.append(1)
-    games[id-1].add_player(Player(len(players),username,request.sid))
+    games[id-1].add_player(Player(len(players), username, request.sid))
     join_room(id)
-    server.socketio.emit('room_message',f'{username} has intered on room {id}',to=id)
-    
+    server.socketio.emit(
+        'room_message', f'{username} has intered on room {id}', to=id)
+
 
 @server.socketio.on('send message')
 def send_room_message(data):
@@ -49,5 +57,7 @@ def handle_message(data):
     messageCount += 1
     data['id'] = messageCount
 
+    room = int(data['room'])
+
     # by including include_self=False, the message wont be sent to the client that sent the message
-    server.socketio.emit('new_message', data)
+    server.socketio.emit('new_message', data, to=room)
