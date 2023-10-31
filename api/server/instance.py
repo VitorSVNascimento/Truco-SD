3f812a5dd8aca4cf217a64d0f8fd74bd40769d77
VitorNasc8 <vitornasc8k13@gmail.com>
2023-10-31 18:23:17 -0300

    Libração da politica de CORS para todas as origens
    - instancy.py: Adição do modificador de CORS ao atributo app
    
    - requirements.txt: adição da biblioteca flask_cors na versão 4.0.0

--- api/server/instance.py
+++ api/server/instance.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from flask_socketio import SocketIO
+from flask_cors import CORS
 import pathlib
 
 from constants.server_constants import *
@@ -8,6 +9,7 @@ from constants.server_constants import *
 class Server():
     def __init__(self,) -> None:
         self.app = Flask(__name__,template_folder=str(pathlib.Path(__file__).parent / "../templates"))
+        CORS(self.app, resources={r"/*": {"origins": "*"}})
         self.api = Api(self.app,
                        version='1.0',
                        title='Truco API',
