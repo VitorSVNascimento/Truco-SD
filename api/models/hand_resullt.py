fd36431503fc2fb5d87d4944e9f5ec51da52c529
VitorNasc8 <vitornasc8k13@gmail.com>
2023-12-03 11:30:27 -0300

    Continuação da lógica da Mão

---
+++ api/models/hand_resullt.py
@@ -0,0 +1,6 @@
+class HandResult:
+
+    def __init__(self, team_winner,hand_value,next_player):
+        self.team_winner = team_winner
+        self.hand_value = hand_value
+        self.next_player = next_player
