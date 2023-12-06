42ed5da3902ccaecd3ff2584f04a11f14b461d6d
Vinícius Pires <viniciusjps15@gmail.com>
2023-12-06 11:45:22 -0300

    Mesa, botão de truco e outras alterações na sala do jogo

--- frontend/src/components/gameRoom/GameRoomPlayer.tsx
+++ frontend/src/components/gameRoom/GameRoomPlayer.tsx
@@ -1,24 +1,24 @@
-export default function GameRoomPlayer(props: { playerName: string, cardsNumber: any }) {
-    const imageSrc = "http://127.0.0.1:8000/static/img/back.png"
+import { Icon } from "@mui/material";
 
+export default function GameRoomPlayer(props: { playerName: string; cardsNumber: any }) {
+	const imageSrc = `${props.cardsNumber}-cards.svg`
+	
 	return (
 		<>
-			<div className="relative left-1/2">
-				{Array.from({ length: props.cardsNumber}, (_, index) => (
-					<div key={index}>
-						<img
-							className={`absolute 
-														${
-															(index == 0 && "z-1 left-3 top-1") ||
-															(index == 1 && "z-2 left-2 top-2") ||
-															(index == 2 && "z-3 left-1 top-3")
-														}  
-														w-10 -translate-x-1/2 transform p-0 md:w-16`}
-							src={imageSrc}
-							alt={`Repeated Image ${index}`}
-						/>
+			<div className="flex items-center justify-center gap-2">
+				<div className="block items-center justify-center">
+					<div className="flex items-center justify-center">
+						<Icon>person</Icon>
 					</div>
-				))}
+					<p className="blockfont-mono text-1xl font-semibold antialiased md:text-2xl">
+						{props.playerName}
+					</p>
+				</div>
+				<img
+					className="w-7 p-0 m-1 md:w-10"
+					src={imageSrc}
+					alt={`Cartas do jogador ${props.playerName}.`}
+				/>
 			</div>
 		</>
 	)
--- frontend/src/views/GameRoom.tsx
+++ frontend/src/views/GameRoom.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react"
 import { useLocation } from "react-router-dom"
 import Chat from "./Chat"
+import { playAudio } from "../utils/utils"
 import Icon from "@mui/material/Icon"
+import GameRoomPlayer from "../components/gameRoom/GameRoomPlayer"
 
 export default function gameRoom() {
 	const location = useLocation()
 	const { props } = location.state || {}
-	const [cards] = useState(props.cards)
-	// const [playersNames] = useState(props.playersNames)
+	const [ cards ] = useState(props.cards)
+	const [ username ] = useState(props.username)
+	// const [ playersNames ] = useState(props.playersNames)
+	const [ roundOrder ] = useState(props.roundOrder)
 	const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
+
+	const TRICK_AUDIO = 7
 
 	const toggleChat = () => {
 		const chat = document.querySelector("#chat")
@@ -19,7 +25,17 @@ export default function gameRoom() {
 
 	const throwCard = () => {
 		console.log("Players", props.playersNames)
+		console.log("Cartas", cards)
 	}
+
+	const callTruco = () => {
+		playAudio(`sounds/truco-${Math.floor(Math.random() * TRICK_AUDIO)}.mp3`)
+		console.log("TRUCOOOOOOOOOO")
+	}
+
+	const isMyTurn = () => {
+		return roundOrder.length > 0 && username === roundOrder[0];
+	};
 
 	return (
 		<div className="min-h-screen bg-gradient-to-bl from-blue-700 via-blue-800 to-slate-700 text-white/90 md:grid md:grid-cols-5 md:content-normal md:gap-4 md:bg-white/90">
@@ -30,71 +46,66 @@ export default function gameRoom() {
 						<div className="m-0 grid h-full grid-rows-6 gap-2 p-0 ">
 							<div className="row-span-1 items-center justify-center">
 								<div className="grid h-full grid-cols-3">
-									<div className="col-span-1 items-center justify-center"></div>
 									<div className="col-span-1 items-center justify-center">
-										
+
 									</div>
-									<div className="col-span-1 items-center justify-center"></div>
+									<div className="col-span-1 flex items-center justify-center">
+										<GameRoomPlayer playerName="ZeLinguica" cardsNumber={3}/>
+									</div>
+									<div className="col-span-1 items-center justify-center">
+
+									</div>
 								</div>
 							</div>
+
 							<div className="row-span-3">
 								<div className="grid h-full grid-cols-6">
-									<div className="col-span-1 items-center justify-center">
-										{/* <div className="relative left-1/2">
-											{Array.from({ length: repeatCount }, (_, index) => (
-												<div key={index}>
-													<img
-														className={`absolute 
-													${
-														(index == 0 && "z-1 left-3 top-1") ||
-														(index == 1 && "z-2 left-2 top-2") ||
-														(index == 2 && "z-3 left-1 top-3")
-													}  
-													w-10 -translate-x-1/2 transform p-0 md:w-16`}
-														src={imageSrc}
-														alt={`Repeated Image ${index}`}
-													/>
-												</div>
-											))}
-										</div> */}
+									<div className="col-span-1 flex items-center justify-center">
+										<GameRoomPlayer playerName="JaoBaitola" cardsNumber={1}/>
 									</div>
-									<div className="col-span-4"></div>
-									<div className="col-span-1">
-										{/* <div className="relative left-1/2">
-											{Array.from({ length: repeatCount }, (_, index) => (
-												<div key={index}>
-													<img
-														className={`absolute 
-														${
-															(index == 0 && "z-1 left-3 top-1") ||
-															(index == 1 && "z-2 left-2 top-2") ||
-															(index == 2 && "z-3 left-1 top-3")
-														}  
-														w-10 -translate-x-1/2 transform p-0 md:w-16`}
-														src={imageSrc}
-														alt={`Repeated Image ${index}`}
-													/>
-												</div>
-											))}
-										</div> */}
+									<div className="col-span-4 bg-green-500 border-4 border-orange-400 rounded-full">
+										
+									</div> 
+									<div className="col-span-1 flex items-center justify-center">
+										<GameRoomPlayer playerName="Chiquinha" cardsNumber={2}/>
 									</div>
 								</div>
 							</div>
+
 							<div className="row-span-2">
-								<div className="row-span-1  flex items-center justify-center">
-									{cards.map((c: { code: null | undefined; url_image: string }, index: number) => (
+								<div className="grid h-full grid-cols-3">
+									<div className="row-span-1 flex items-center justify-center">
 										<button
-											className={`m-1 w-16 transform transition-transform hover:translate-y-[-7px] md:w-28 ${
-												hoveredIndex !== null && hoveredIndex !== index ? "blur-sm grayscale" : ""
-											}`}
-											key={c.code}
-											onMouseEnter={() => setHoveredIndex(index)}
-											onMouseLeave={() => setHoveredIndex(null)}
-											onClick={throwCard}
+											id="trucoButton"
+											className="flex rounded-full bg-blue-500 p-3 text-gray-50 shadow-sm focus-within:bg-red-700 focus-within:outline hover:bg-red-600 disabled:bg-gray-500"
+											type="button"
+											onClick={callTruco}
+											disabled={!isMyTurn()}
 										>
-											<img className="img-responsive" src={c.url_image} />
+											TRUUUUUUUCO
 										</button>
-									))}
+										
+									</div>
+									<div className="row-span-1 flex items-center justify-center">
+										{cards.map((c: { code: null | undefined; url_image: string }, index: number) => (
+											<button
+												className={`m-1 w-16 md:w-28 ${isMyTurn() ? "transform transition-transform hover:translate-y-[-7px]": ""} ${
+													(!isMyTurn()) ? "grayscale opacity-80" : ((hoveredIndex !== null && hoveredIndex !== index) ? "blur-sm grayscale" : "")
+												}`}
+												key={c.code}
+												onMouseEnter={() => setHoveredIndex(index)}
+												onMouseLeave={() => setHoveredIndex(null)}
+												onClick={throwCard}
+												disabled={!isMyTurn()}
+											>
+												<img className="img-responsive" src={c.url_image} />
+											</button>
+										))}
+									</div>
+									<div className="row-span-1 flex items-center justify-center">
+										<div className="blockfont-mono antialiased font-semibold text-2xl md:text-4xl">{username}</div>
+										{isMyTurn() && (<div className="blockfont-mono antialiased font-semibold text-1xl md:text-2xl">Sua vez!</div>)}	
+									</div>
 								</div>
 							</div>
 						</div>
